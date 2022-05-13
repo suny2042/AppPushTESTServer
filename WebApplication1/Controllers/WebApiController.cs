dc10053dc041bccba8380079c6ea0b5605a88af9
suny2042 <suny2042@naver.com>
2022-05-13 16:14:00 +0900

    초기 커밋.

---
+++ WebApplication1/Controllers/WebApiController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Buffers;
+using System.Text;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebApiController : ControllerBase
+    {
+        //브라우저에서 전달받은 Instnace ID Token 저장
+        [Route("~/api/save-token"), HttpPost]
+        //[Route("save-token"), HttpPost]
+        public async Task<ActionResult> SaveSubscription()
+        {
+            try
+            {
+                //1. Instnace ID Token 조회
+                string pPostBody = "";
+
+                using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+                {
+                    pPostBody = await reader.ReadToEndAsync();
+                }
+
+                string[] lPostBody = pPostBody.Split('|');
+
+                string lBodyContent = HttpUtility.UrlEncode("앱 푸쉬 내용입니다.");
+                Console.WriteLine(lBodyContent);
+                // 푸쉬 전송
+                await sendMessage(lPostBody[1], lBodyContent, lPostBody[0]);
+
+
+                //HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, new { Success = true });
+                //return response;
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                //HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.BadRequest, new { Success = false, Error = error });
+                //return response;
+                return Problem(error);
+            }
+        }
+
+
+
+        //Google FCM 서버에 Push Message 발송요청
+        public async Task sendMessage(string title, string msg, string instanceIdToken)
+        {
+            //1. 전송정보 생성
+            string serverKey = "key=AAAAOUKni2g:APA91bFducLWEwYQHwPcLFGXit-GX6P5bIDmCqLqZp7RYL3EaLFv8kl8w9Hz2w5Q8supUykxfUZZzdhgZfVvY1nMvZy3BaWrt5DXTfNw5eyK2ymsoySfBDriZR4mdYdZKO7rIt__a9TT";
+            string url = "https://fcm.googleapis.com/fcm/send";
+            string postJson = (@"{
+                            'content_available': false,
+                            'data':
+                            {
+                                'title': '" + title + @"',
+                                'body': '" + msg + @"'
+
+                            },
+                            'to': '" + instanceIdToken + @"'
+                        }")
+                                .Replace("'", "\"")
+                                .Trim();
+
+            //2. Firebase 서버에 REST 전송
+            using (HttpClient client = new HttpClient() { BaseAddress = new Uri(url) })
+            {
+                //1) 요청
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", serverKey);
+                StringContent postEncoded = new StringContent(postJson.Replace("\t", "").Replace("\r\n", ""), Encoding.UTF8, "application/json");
+                HttpResponseMessage httpResponse = await client.PostAsync("", postEncoded);
+
+                //2) 응답분석
+                string responseText = await httpResponse.Content.ReadAsStringAsync();
+                if (httpResponse.IsSuccessStatusCode)
+                    Console.WriteLine(responseText);
+                    //Debug.WriteLine("전송성공", responseText);
+                else
+                    Console.WriteLine(responseText);
+                    //Debug.WriteLine("전송실패:", responseText, httpResponse.StatusCode);
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/wwwroot/service-worker.js
@@ -0,0 +1,50 @@
+﻿//Push Message 수신 이벤트
+self.addEventListener('push', function (event) {
+    console.log('[ServiceWorker] 푸시알림 수신: ', event);
+
+
+    var obj = event.data.json();
+
+    console.log(obj.data.title);
+    console.log(obj.data.body);
+
+
+    //Push 정보 조회
+    var title = '알림 ' + (decodeURIComponent(obj.data.title.replaceAll("+", "%20"))) || '알림 내용없음';
+    var body = (decodeURIComponent(obj.data.body.replaceAll("+", "%20")));
+    //var icon = event.data.icon || '/Images/icon.png'; //512x512
+    //var badge = event.data.badge || '/Images/badge.png'; //128x128
+    var options = {
+        body: body
+        //,
+        //icon: icon,
+        //badge: badge
+    };
+
+    //Notification 출력
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+//사용자가 Notification을 클릭했을 때
+self.addEventListener('notificationclick', function (event) {
+    console.log('[ServiceWorker] 푸시알림 클릭: ', event);
+
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: "window" })
+            .then(function (clientList) {
+                //실행된 브라우저가 있으면 Focus
+                for (var i = 0; i < clientList.length; i++) {
+                    var client = clientList[i];
+                    if (client.url == '/' && 'focus' in client)
+                        return client.focus();
+                }
+                //실행된 브라우저가 없으면 Open
+                if (clients.openWindow)
+                    return clients.openWindow(self.location.origin);
+            })
+    );
+});
+
+console.log('[ServiceWorker] 시작');
+
